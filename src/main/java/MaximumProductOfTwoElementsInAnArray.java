ca8ca08262d6c39822057db7ea13af120a8f76b4
xwj31 <>
2020-06-18 17:18:40 +0100

    added leetcode

---
+++ src/main/java/MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,25 @@
+public class MaximumProductOfTwoElementsInAnArray {
+
+    public int maxProduct(int[] nums) {
+
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for(int num : nums){
+            if(num> max){
+                secondMax = max;
+                max = num;
+            }
+            else if(num > secondMax) secondMax = num;
+        }
+        return (max - 1) * (secondMax - 1);
+
+    }
+
+    public static void main(String[] args) {
+        MaximumProductOfTwoElementsInAnArray maximumProductOfTwoElementsInAnArray = new MaximumProductOfTwoElementsInAnArray();
+        System.out.println(maximumProductOfTwoElementsInAnArray.maxProduct(new int[]{3,4,5,2}) == 12 ? "Test passed" : "Test failed");
+        System.out.println(maximumProductOfTwoElementsInAnArray.maxProduct(new int[]{1, 5, 4, 5}) == 16 ? "Test passed" : "Test failed");
+        System.out.println(maximumProductOfTwoElementsInAnArray.maxProduct(new int[]{3,7}) == 12 ? "Test passed" : "Test failed");
+    }
+}
