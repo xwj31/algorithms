4fa0498a5ede5ac6b2a581a8cb720635684e9457
xwj31 <>
2020-09-21 14:27:22 +0100

    more algos + the fabled fizzbuzz

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public List<String> fizzBuzz(int n) {
+
+            // ans list
+            List<String> ans = new ArrayList<String>();
+
+            for (int num = 1; num <= n; num++) {
+
+                boolean divisibleBy3 = (num % 3 == 0);
+                boolean divisibleBy5 = (num % 5 == 0);
+
+                if (divisibleBy3 && divisibleBy5) {
+                    // Divides by both 3 and 5, add FizzBuzz
+                    ans.add("FizzBuzz");
+                } else if (divisibleBy3) {
+                    // Divides by 3, add Fizz
+                    ans.add("Fizz");
+                } else if (divisibleBy5) {
+                    // Divides by 5, add Buzz
+                    ans.add("Buzz");
+                } else {
+                    // Not divisible by 3 or 5, add the number
+                    ans.add(Integer.toString(num));
+                }
+            }
+
+            return ans;
+        }
+
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.fizzBuzz(3));
+    }
+}
--- src/main/java/ReformatTheString.java
+++ src/main/java/ReformatTheString.java
@@ -1,36 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class ReformatTheString {
 
     public String reformat(String s) {
 
-        char[] chars = s.toCharArray();
+        if(s == null || s.length()<=1)
+            return s;
+        List<Character> digits = new ArrayList<>();
+        List<Character> chars = new ArrayList<>();
 
-        for (int i = 0; i < chars.length; i++) {
-            if (isNumeric(chars[i]) && isNumeric(chars[i+1])) {
-
-            }
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)) digits.add(c);
+            else chars.add(c);
         }
-        return s;
-    }
+        if(Math.abs(digits.size()-chars.size()) >= 2) return "";
 
-    public static boolean isNumeric(char str) {
-        try {
-            Double.parseDouble(String.valueOf(str));
-            return true;
-        } catch(NumberFormatException e){
-            return false;
+        StringBuilder sb = new StringBuilder();
+        boolean digitOrChar = digits.size() >= chars.size();
+        for(int i = 0; i<s.length(); i++){
+            if(digitOrChar)
+                sb.append(digits.remove(0));
+            else
+                sb.append(chars.remove(0));
+            digitOrChar = !digitOrChar;
         }
-    }
-
-    public static char[] swap(String str, int i, int j) {
-        char ch[] = str.toCharArray();
-        char temp = ch[i];
-        ch[i] = ch[j];
-        ch[j] = temp;
-        return ch;
+        return sb.toString();
     }
 
     public static void main(String[] args) {
         ReformatTheString reformatTheString = new ReformatTheString();
+        System.out.println(reformatTheString.reformat("ab123").equals("3b2a1") ? "test passed" : "test failed");
+        System.out.println(reformatTheString.reformat("covid2019").equals("") ? "test passed" : "test failed");
         System.out.println(reformatTheString.reformat("a0b1c2").equals("a0b1c2") ? "Test passed" : "Test failed");
         System.out.println(reformatTheString.reformat("leetcode").equals("") ? "Test passed" : "Test failed");
         System.out.println(reformatTheString.reformat("1229857369").equals("") ? "Test passed" : "Test failed");
